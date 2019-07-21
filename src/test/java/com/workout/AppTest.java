9bc471315116faf2d1ac64beebc6632d0558c2f4
BSP <47625232+itzbsp@users.noreply.github.com>
2019-07-21 18:55:27 +0530

    Update AppTest.java

--- src/test/java/com/workout/AppTest.java
+++ src/test/java/com/workout/AppTest.java
@@ -19,7 +19,7 @@ public class AppTest {
 }
 */
 
-package com.workout;
+/*package com.workout;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -42,6 +42,43 @@ public void TestRun()
 	driver.findElement(By.id("login_btn1")).click();
 	System.out.println(driver.getCurrentUrl());
 	System.out.println("h ha ha");
+	driver.quit();
+}
+}*/
+
+
+
+
+package com.workout;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.*;
+import org.junit.Test;
+import org.openqa.selenium.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxBinary;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+
+import java.util.concurrent.TimeUnit;
+
+public class AppTest {
+WebDriver driver;
+@Test
+public void TestRun()
+{
+	FirefoxBinary firefoxBinary = new FirefoxBinary();
+	//firefoxBinary.addCommandLineOptions("--headless");
+	System.setProperty("webdriver.gecko.driver","/usr/bin/geckodriver");
+	driver=new FirefoxDriver();
+	driver.get("http://localhost:5151/WorkOutFinal/");
+	//System.out.println(driver.getTitle());
+	driver.findElement(By.id("userName")).sendKeys("bsp@demo.com");
+	driver.findElement(By.id("userPw")).sendKeys("bsp");
+	driver.findElement(By.id("login_btn1")).click();
+	//System.out.println(driver.getTitle());
 	driver.quit();
 }
 }
